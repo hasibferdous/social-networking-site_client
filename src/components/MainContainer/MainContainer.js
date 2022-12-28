7dfd47cd4f3f304ee1499f39d000c6726b50dbda
hasibferdous <hasibferdous.ict@gmail.com>
2022-12-29 01:10:01 +0600

    posting image added

--- src/components/MainContainer/MainContainer.js
+++ src/components/MainContainer/MainContainer.js
@@ -1,11 +1,25 @@
-import React, { useContext } from 'react';
+import React, { useContext, useRef, useState } from 'react';
 import { AuthContext } from '../../contexts/AuthProvider';
 import './maincontainer.css';
 
 import { BiImageAdd} from 'react-icons/bi';
 import { AiOutlineVideoCameraAdd} from 'react-icons/ai';
+import { ImCross} from 'react-icons/im';
+
 const MainContainer = () => {
     const {user} = useContext(AuthContext);
+
+    const [image, setImage] = useState(null);
+    const imageRef = useRef();
+
+    const onImageChange = (event) => {
+        if (event.target.files && event.target.files[0]){
+            let img = event.target.files[0];
+            setImage({
+                image: URL.createObjectURL(img),
+            });
+        }
+    };
 
     return (
         <div className='h-96 w-full bg-indigo-200 my-5 mx-5'>
@@ -20,7 +34,7 @@ const MainContainer = () => {
             </div>
             </div>
             <div className='option'>
-                <div>
+                <div onClick={()=>imageRef.current.click()}>
                     <BiImageAdd className='w-16 h-16'/>Image
                 </div>
                 <div>
@@ -29,7 +43,21 @@ const MainContainer = () => {
                 <div>
                     <button className='button px-5 bg-indigo-500 rounded-md text-white font-bold ml-40 mt-2 w-22 h-11'>Share</button>
                 </div>
+                <div style={{display: 'none'}} >
+                    <input 
+                    type="file"
+                    name='myImage'
+                    ref={imageRef}
+                    onChange={onImageChange} />
+
+                </div>
             </div>
+            {image && (
+                <div className='previewImage ml-32 w-60 h-60'>
+                    <ImCross onClick={()=>setImage(null)}/>
+                    <img src={image.image} alt=""/>
+                </div>
+            )}
         </div>
     );
 };
